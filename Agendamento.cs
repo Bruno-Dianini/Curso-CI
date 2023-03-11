3df7924f1efedf8560f3b1caff2ac5775fc9a094
Bruno <bdainini@gmail.com>
2023-03-11 15:35:59 -0300

    reestruturando codigo

--- Agendamento.cs
+++ Agendamento.cs
@@ -1,88 +1,46 @@
 using System;
-using System;
+using System.Collections.Generic;
 
 public class Agendamento
 {
     public int escolha;
+     Dictionary<DateTime, string> agenda = new Dictionary<DateTime, string>();
 
     public void dataAgenda()
     {
-         Console.WriteLine("Qual Equipamento vocé gotaria de agendar?");
-            Console.WriteLine("[1] TV [2] TV com VCR [3] Projetor de slides [4]Sistemas de áudio-microfone [5]Caixa amplificada [6]Notebooks");
-            
-            escolha = Convert.ToInt32(Console.ReadLine());
+       while (true)
+            {
+            Console.WriteLine("Digite a data (dd/mm/aaaa) ou digite 'sair' para encerrar:");
+            string input = Console.ReadLine();
 
-        switch(escolha)
-        {
-                
-            case 1:
-                string? data = ""; 
-                Console.WriteLine("Qual a data que deseja agendar:");
-                data = Console.ReadLine();
+            if (input.ToLower() == "sair")
                 break;
-            case 2:
-                string? data2 = ""; 
-                Console.WriteLine("Qual a data que deseja agendar:");
-                data2 = Console.ReadLine();
-                break;
-            case 3:
-                string? data3 = ""; 
-                Console.WriteLine("Qual a data que deseja agendar:");
-                data3 = Console.ReadLine();
-                break;
-            case 4:
-                string? data4 = ""; 
-                Console.WriteLine("Qual a data que deseja agendar:");
-                data4 = Console.ReadLine();
-                break;
-            case 5:
-                string? data5 = ""; 
-                Console.WriteLine("Qual a data que deseja agendar:");
-                data5 = Console.ReadLine();
-                break;
-            case 6:
-                string? data6 = ""; 
-                Console.WriteLine("Qual a data que deseja agendar:");
-                data6 = Console.ReadLine();
-                break;
-            default:
-                escolha = -1;
-                break;
+
+            if (DateTime.TryParse(input, out DateTime data))
+            {
+                if (agenda.ContainsKey(data))
+                {
+                    Console.WriteLine("Data já agendada!");
+                }
+                else
+                {
+                    Console.WriteLine("Digite o evento:");
+                    string evento = Console.ReadLine();
+                    agenda.Add(data, evento);
+                    Console.WriteLine("Evento agendado!");
+                }
             }
-            
-          
-
-        if(escolha <= 0 )
-        {
-            Console.WriteLine("Equipamento enexistente");
-        }
-        else if (escolha == 1)
-        {
-            Console.WriteLine("Equipamento registrado para retirada ");
-        }
-            else if (escolha == 2)
-        {
-            Console.WriteLine("Equipamento registrado para retirada ");
-        }
-            else if (escolha == 3)
-        {
-            Console.WriteLine("Equipamento registrado para retirada");
-        }
-            else if (escolha == 4)
-        {
-            Console.WriteLine("Equipamento registrado para retirada");
-        }
-            else if (escolha == 5)
-        {
-            Console.WriteLine("Equipamento registrado para retirada");
-        }
-            else if (escolha == 6)
-        {
-            Console.WriteLine("Equipamento registrado para retirada");
+            else
+            {
+                Console.WriteLine("Data inválida!");
+            }
         }
 
-    }
-    
+        Console.WriteLine("\nAgenda:");
+
+        foreach (KeyValuePair<DateTime, string> item in agenda)
+        {
+            Console.WriteLine("{0}: {1}", item.Key.ToShortDateString(), item.Value);
+        }
+        }
 }
-
-
